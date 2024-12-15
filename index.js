f0ccb7b981e76d9c8decd9b67d54146810a1ccaf
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-12-15 13:21:56 +0600

    get data from bd done

--- index.js
+++ index.js
@@ -28,6 +28,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const jobsCollection = client.db("jobPortal").collection("jobs");
+
+    app.get("/jobs", async(req, res) => {
+        const result = await jobsCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
